c99bfce9917fb04feaeb6a29dc0a9386587ee5b9
Carl Graving <carl@projectronin.com>
2023-10-18 07:33:09 -0400

    Add a filtered get authors by type

--- common-data/src/main/kotlin/com/projectronin/common/data/models/DocumentReferenceExt.kt
+++ common-data/src/main/kotlin/com/projectronin/common/data/models/DocumentReferenceExt.kt
@@ -2,6 +2,7 @@ package com.projectronin.common.data.models
 
 import com.projectronin.fhir.r4.Attachment
 import com.projectronin.fhir.r4.DocumentReference
+import com.projectronin.fhir.r4.Reference
 
 val DocumentReference.patientId: String
     get() = requireNotNull(subject?.patientId) { "Subject is null" }
@@ -12,3 +13,13 @@ val DocumentReference.tenantId: String
 fun DocumentReference.getUrlAttachments(): List<Attachment> = content
     .filter { it.attachment.isUrlAttachment }
     .map { content -> content.attachment }
+
+fun DocumentReference.getAuthorsByType(type: String? = null): List<Reference> {
+    return this.author?.let {
+        when (type) {
+            is String -> it.filter { author: Reference -> author.type?.equals(type, true) ?: false }
+            else -> it
+        }
+    }?.toList() ?: emptyList()
+}
+
--- common-data/src/test/kotlin/com/projectronin/common/data/models/DocumentReferenceExtKtTest.kt
+++ common-data/src/test/kotlin/com/projectronin/common/data/models/DocumentReferenceExtKtTest.kt
@@ -14,8 +14,8 @@ class DocumentReferenceExtKtTest {
     private val patientId = "apposnd-e63wRTbPfr1p8UW81d8Seiw3"
     private val dataAuthority = "EHR Data Authority"
     private val url = "https://images.squarespace-cdn.com/content/v1/" +
-        "642adca97e8fbd450fafb96c/dccea417-fac7-4641-b301-8ccb2772f912/" +
-        "Ronin+Logo+-+White+Text+Transparent+Bg+-+Large.png?format=1500w"
+            "642adca97e8fbd450fafb96c/dccea417-fac7-4641-b301-8ccb2772f912/" +
+            "Ronin+Logo+-+White+Text+Transparent+Bg+-+Large.png?format=1500w"
 
     private val docRef = DocumentReference().apply {
         subject = Reference().apply {
@@ -48,6 +48,16 @@ class DocumentReferenceExtKtTest {
                 }
             }
         )
+        author = listOf(
+            Reference().apply {
+                type = "Provider"
+                reference = "Provider/some-provider-id"
+            },
+            Reference().apply {
+                type = "NotProvider"
+                reference = "NotProvider/some-not-provider-id"
+            }
+        )
     }
 
     @Test
@@ -65,5 +75,17 @@ class DocumentReferenceExtKtTest {
         val urlAttachments = docRef.getUrlAttachments()
         assertThat(urlAttachments.size).isEqualTo(1)
         assertThat(urlAttachments[0].url).isEqualTo(url)
+    }
+
+    @Test
+    fun getAuthorsByTypeTest() {
+        val providerAuthors = docRef.getAuthorsByType("Provider")
+        assertThat(providerAuthors.size).isEqualTo(1)
+        assertThat(providerAuthors[0].reference).isEqualTo("Provider/some-provider-id")
+
+        val allAuthors = docRef.getAuthorsByType()
+        assertThat(allAuthors.size).isEqualTo(2)
+        assertThat(allAuthors[0].reference).isEqualTo("Provider/some-provider-id")
+        assertThat(allAuthors[1].reference).isEqualTo("NotProvider/some-not-provider-id")
     }
 }
