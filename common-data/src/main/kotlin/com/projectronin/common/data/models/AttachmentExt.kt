ebff508dd9240b42c1726a26e9b21e0921da9a76
Brent Beardsley <brent@projectronin.com>
2023-11-08 15:18:33 -0700

    add url attachment ehrdaUrl extensions methods

--- common-data/src/main/kotlin/com/projectronin/common/data/models/AttachmentExt.kt
+++ common-data/src/main/kotlin/com/projectronin/common/data/models/AttachmentExt.kt
@@ -1,6 +1,49 @@
 package com.projectronin.common.data.models
 
 import com.projectronin.fhir.r4.Attachment
+import com.projectronin.fhir.r4.Element
+import com.projectronin.fhir.r4.Extension
+import java.net.URL
 
 val Attachment.isUrlAttachment: Boolean
     get() = url != null
+
+val Attachment.ehrdaUrlOrNull: URL?
+    get() = run {
+        require(isUrlAttachment) { "not a url attachment" }
+        _url?.extension?.findEhrdaAttachmentUrlOrNull()?.valueUrl?.let { URL(it) }
+    }
+
+val Attachment.ehrdaUrl: URL
+    get() = requireNotNull(ehrdaUrlOrNull) { "ehrdaUrl was not found" }
+
+fun Attachment.setEhrdaUrl(url: URL): Attachment {
+    require(isUrlAttachment) { "not a url attachment" }
+    if (this._url == null) {
+        this._url = Element()
+    }
+
+    val urlElement = requireNotNull(this._url) { "_url is not found" }
+    if (urlElement.extension == null) {
+        this.extension = mutableListOf()
+    }
+
+    val extensionElement = requireNotNull(urlElement.extension) { "_url extension not found" }
+    extensionElement.findEhrdaAttachmentUrlOrNull()?.also {
+        require(extensionElement.remove(it)) { "failed to remove existing ehrdaAttachmentUrl" }
+    }
+
+    extensionElement.add(buildEhrdaAttachmentUrl(url))
+    return this
+}
+
+private fun buildEhrdaAttachmentUrl(valueURL: URL) = Extension().apply {
+    this.url = EHRDA_ATTACHMENT_URL
+    this.valueUrl = valueURL.toString()
+    this.extension = emptyList()
+}
+
+private fun MutableList<Extension>.findEhrdaAttachmentUrlOrNull() =
+    firstOrNull { it.url == EHRDA_ATTACHMENT_URL }
+
+private const val EHRDA_ATTACHMENT_URL = "http://projectronin.io/fhir/StructureDefinition/Extension/ehrdaAttachmentURL"
--- common-data/src/test/kotlin/AttachmentExtKtTest.kt
+++ common-data/src/test/kotlin/AttachmentExtKtTest.kt
@@ -1,23 +1,117 @@
+import com.projectronin.common.data.models.ehrdaUrl
+import com.projectronin.common.data.models.ehrdaUrlOrNull
 import com.projectronin.common.data.models.isUrlAttachment
+import com.projectronin.common.data.models.setEhrdaUrl
 import com.projectronin.fhir.r4.Attachment
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+import java.net.URL
 
 class AttachmentExtKtTest {
 
-    @Test
-    fun `test is url attachment`() {
-        val a = Attachment().apply {
-            url = "asdf"
+    @Nested
+    inner class IsUrlAttachment {
+        @Test
+        fun `test is url attachment`() {
+            assertThat(validAttachment().isUrlAttachment).isTrue()
         }
-        assertThat(a.isUrlAttachment).isEqualTo(true)
+
+        @Test
+        fun `test is not url attachment`() {
+            assertThat(nonUrlAttachment().isUrlAttachment).isFalse()
+        }
     }
 
-    @Test
-    fun `test is not url attachment`() {
-        val a = Attachment().apply {
-            url = null
+    @Nested
+    inner class EhrdaUrlOrNull {
+        @Test
+        fun `throws error when not attachment`() {
+            val message = assertThrows<IllegalArgumentException> { nonUrlAttachment().ehrdaUrl }.message
+
+            assertThat(message).isEqualTo("not a url attachment")
         }
-        assertThat(a.isUrlAttachment).isEqualTo(false)
+
+        @Test
+        fun `works when null`() {
+            assertThat(validAttachment().ehrdaUrlOrNull).isNull()
+        }
+
+        @Test
+        fun `works when not null`() {
+            val expectedUrl = URL("http://foo")
+
+            assertThat(validAttachment().setEhrdaUrl(expectedUrl).ehrdaUrlOrNull).isEqualTo(expectedUrl)
+        }
     }
+
+    @Nested
+    inner class EhrdaUrl {
+        @Test
+        fun `throws error when not attachment`() {
+            val message = assertThrows<IllegalArgumentException> { nonUrlAttachment().ehrdaUrl }.message
+
+            assertThat(message).isEqualTo("not a url attachment")
+        }
+
+        @Test
+        fun `throws error when null`() {
+            val message = assertThrows<IllegalArgumentException> { validAttachment().ehrdaUrl }.message
+
+            assertThat(message).isEqualTo("ehrdaUrl was not found")
+        }
+
+        @Test
+        fun `works when not null`() {
+            val expectedUrl = URL("http://foo")
+
+            assertThat(validAttachment().setEhrdaUrl(expectedUrl).ehrdaUrl).isEqualTo(expectedUrl)
+        }
+    }
+
+    @Nested
+    inner class SetEhrdaUrl {
+        @Test
+        fun `throws error when not attachment`() {
+            val message = assertThrows<IllegalArgumentException> {
+                nonUrlAttachment().setEhrdaUrl(URL("http://foo"))
+            }.message
+
+            assertThat(message).isEqualTo("not a url attachment")
+        }
+
+        @Test
+        fun `works when no existing value`() {
+            // This is tested in the other unit tests
+        }
+
+        @Test
+        fun `works when already has existing value`() {
+            val a = validAttachment()
+                .setEhrdaUrl(URL("https://one"))
+                .setEhrdaUrl(URL("https://two"))
+                .setEhrdaUrl(URL("https://three"))
+
+            assertThat(a.ehrdaUrl).isEqualTo(URL("https://three"))
+
+            val extension = a._url?.extension ?: fail("_url.extension was not set")
+
+            assertThat(extension).hasSize(1)
+            with(extension.first()) {
+                assertThat(this.url).isEqualTo("http://projectronin.io/fhir/StructureDefinition/Extension/ehrdaAttachmentURL")
+                assertThat(this.valueUrl).isEqualTo("https://three")
+            }
+        }
+    }
+}
+
+private fun validAttachment() = attachment("http://asdf")
+private fun nonUrlAttachment() = attachment()
+
+private fun attachment(url: String? = null, init: Attachment.() -> Unit = {}) = Attachment().apply {
+    this.url = url?.let { URL(it) }?.toString()
+    init()
 }
